d5e5be65d8a7d63645111a51ed7bea069781057c
varalakshmi <varalakshmi@77.com>
2025-04-13 02:57:48 +0530

    Setting Up Things

---
+++ App.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client"
+
+const App =()=>{
+    return(
+        <>
+        <h1 className="text-5xl font-bold bg-amber-300 text-red-600">NetFlex Creation</h1>
+        </>
+    )
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
+
