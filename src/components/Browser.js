9805f939e43e88d4f2049491c6566365534e391c
varalakshmi <varalakshmi@77.com>
2025-04-16 00:59:50 +0530

    signOut Authentication

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,8 +1,22 @@
+import { useNavigate } from "react-router";
+import { auth } from "../../utils/firebase"
+import { signOut } from "firebase/auth";
+
 export const Browser= ()=>{
+    const navigate = useNavigate()
+    // signOut Authentication
+    const handleSignOut = ()=>{
+        signOut(auth).then(() => {
+            navigate("/")
+          }).catch((error) => {
+            navigate("/error") 
+          });
+    }
     return(
         <>
             <div>
                 <h1>Browser</h1>
+                <button onClick={handleSignOut}>Sign out</button>
             </div>
         </>
     )
